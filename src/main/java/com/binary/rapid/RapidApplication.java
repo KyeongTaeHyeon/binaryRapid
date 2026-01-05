a8cd85ff09bdaa09d1d26c3b6e94ecf8431b8a27
KTH <proxogus@naver.com>
2026-01-05 22:03:02 +0900

    Update RapidApplication and build configuration to improve logging and simplify exclusions

--- src/main/java/com/binary/rapid/RapidApplication.java
+++ src/main/java/com/binary/rapid/RapidApplication.java
@@ -6,16 +6,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication()
 public class RapidApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RapidApplication.class, args);
     }
-
-
+    
 }
