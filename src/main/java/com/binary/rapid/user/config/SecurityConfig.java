d1b8275c517accf0f60fefabb3fd5d87570991b6
KTH <proxogus@naver.com>
2026-01-15 18:27:22 +0900

    feat: add authorization for /origin endpoint

--- src/main/java/com/binary/rapid/user/config/SecurityConfig.java
+++ src/main/java/com/binary/rapid/user/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/",
+                                "/origin",
                                 "/user/LocalSignin",
                                 "/login/register",
                                 "/user/refresh",
